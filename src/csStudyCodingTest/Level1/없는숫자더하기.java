e4c0204a139098be1c8e162d3f8dd069570da83b
CHANG MIN PARK <98qkrckdals@naver.com>
2023-04-02 01:44:09 +0900

    레벨1

--- src/csStudyCodingTest/Level1/없는숫자더하기.java
+++ src/csStudyCodingTest/Level1/없는숫자더하기.java
@@ -6,14 +6,16 @@ public class 없는숫자더하기 {
     public static int solution(int[] numbers) {
         int totalSum = 45;
 
-        for (int i : numbers){
+        for (int i : numbers) {
             totalSum -= i;
         }
         return totalSum;
     }
 
     public static void main(String[] args) {
-        int[] a = {5,8,4,0,6,7,9};
+        int[] a = {5, 8, 4, 0, 6, 7, 9};
         System.out.println(solution(a));
     }
 }
+
+
