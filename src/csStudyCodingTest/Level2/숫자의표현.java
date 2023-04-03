7d02b61e3c8512b72ee3d4448e6b4d2b6eb3a376
CHANG MIN PARK <98qkrckdals@naver.com>
2023-04-03 20:08:08 +0900

    레벨2

--- src/csStudyCodingTest/Level2/숫자의표현.java
+++ src/csStudyCodingTest/Level2/숫자의표현.java
@@ -1,7 +1,5 @@
 package csStudyCodingTest.Level2;
-/*
-* 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 홀수 약수의 개수와 같다라는 정수론 정리가 있습니다(신가하다)
-* */
+
 public class 숫자의표현 {
     public int solution(int n) {
         int answer = 1;
@@ -22,10 +20,19 @@ public class 숫자의표현 {
         }
         return answer;
     }
-
+//     주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수는 주어진 수의 약수 중에 홀수의 개수와 같다라는 정수론 정리가 있습니다(신가하다)
+    public int solution2(int n){
+        int answer = 0;
+        for (int i = 1; i<=n; i++){
+            if (n%i==0){
+                answer++;
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args) {
         숫자의표현 s = new 숫자의표현();
-        System.out.println(s.solution(15));
+        System.out.println(s.solution2(15));
     }
 
 
