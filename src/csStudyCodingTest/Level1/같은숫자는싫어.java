02f684410a7401fcd1191f2b4099e5791fcdfda5
CHANG MIN PARK <98qkrckdals@naver.com>
2023-05-12 02:34:56 +0900

    레벨 1

--- src/csStudyCodingTest/Level1/같은숫자는싫어.java
+++ src/csStudyCodingTest/Level1/같은숫자는싫어.java
@@ -2,7 +2,9 @@ package csStudyCodingTest.Level1;
 
 public class 같은숫자는싫어 {
     public static void main(String[] args) {
-        int a;
-        int b;
+        int a = 1;
+        int b = 2;
+
+        System.out.println(a+b);
     }
 }
