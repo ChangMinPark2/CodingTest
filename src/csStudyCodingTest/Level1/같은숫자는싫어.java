a6cc34362bb806966ca09d7b7c7f21537533aae2
CHANG MIN PARK <98qkrckdals@naver.com>
2023-06-23 01:24:20 +0900

    레벨 1

--- src/csStudyCodingTest/Level1/같은숫자는싫어.java
+++ src/csStudyCodingTest/Level1/같은숫자는싫어.java
@@ -3,41 +3,20 @@ package csStudyCodingTest.Level1;
 import java.util.*;
 
 public class 같은숫자는싫어 {
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
+        public int[] solution(int []arr) {
 
-        Stack<String> a = new Stack<>();
+            Stack<Integer> stack = new Stack<>();
 
-        Stack<Character> c = new Stack<>();
+            for (int i=0; i<arr.length; i++){
+                if (stack.size() ==0 || arr[i] != stack.peek())
+                    stack.push(arr[i]);
+            }
 
-        List<Integer> list = new ArrayList<>();
+            int[] answer = new int[stack.size()];
 
-        List<String> list1 = new ArrayList<>();
-
-        List<Integer> list2 = new LinkedList<>();
-        list2.add();
-        list2.addAll()
-        Set<Integer> a = new HashSet<>();
-        HashMap<Long, LinkedList> a = new HashMap<>();
-        Queue<Integer> a = new Queue<Integer>() {
+            for (int i= stack.size()-1; i>=0; i--){
+                answer[i] = stack.pop();
+            }
+            return answer;
         }
-
-        String i;
-        for(list1 :i){
-
-        }
-
-        list.contains(list);
-
-        System.out.println(a+b);
-        if(a == 2){
-            System.out.println(a);
-        } else System.out.println(b);
-
-
-        if (a == 3){
-            System.out.println(a + b);
-        }
-    }
 }
