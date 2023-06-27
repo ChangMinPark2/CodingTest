d94c42663a10051dcc532e236a375bc0e137bdb9
CHANG MIN PARK <98qkrckdals@naver.com>
2023-06-28 01:08:44 +0900

    해시 : 레벨1

--- src/csStudyCodingTest/Level1/같은숫자는싫어.java
+++ src/csStudyCodingTest/Level1/같은숫자는싫어.java
@@ -3,20 +3,42 @@ package csStudyCodingTest.Level1;
 import java.util.*;
 
 public class 같은숫자는싫어 {
-        public int[] solution(int []arr) {
+//        public int[] solution(int []arr) {
+//
+//            Stack<Integer> stack = new Stack<>();
+//
+//            for (int i=0; i<arr.length; i++){
+//                if (stack.size() ==0 || arr[i] != stack.peek())
+//                    stack.push(arr[i]);
+//            }
+//
+//            int[] answer = new int[stack.size()];
+//
+//            for (int i= stack.size()-1; i>=0; i--){
+//                answer[i] = stack.pop();
+//            }
+//            return answer;
+//        }
+    public static int[] solution(int []arr){
 
-            Stack<Integer> stack = new Stack<>();
+        Set<Integer> set = new HashSet<>();
 
-            for (int i=0; i<arr.length; i++){
-                if (stack.size() ==0 || arr[i] != stack.peek())
-                    stack.push(arr[i]);
-            }
-
-            int[] answer = new int[stack.size()];
-
-            for (int i= stack.size()-1; i>=0; i--){
-                answer[i] = stack.pop();
-            }
-            return answer;
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
         }
+
+        int[] answer = new int[set.size()];
+        int index = 0;
+
+        for (int num : set) {
+            answer[index++] = num;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 1, 1, 2, 2, 2, 3, 4, 4};
+        System.out.println(Arrays.toString(solution(a)));
+    }
 }
---
+++ src/csStudyCodingTest/해시/완주하지못한선수.java
@@ -0,0 +1,28 @@
+package csStudyCodingTest.해시;
+
+import java.util.HashMap;
+
+public class 완주하지못한선수 {
+    //participant -> 마라톤에 참여한 선수 이름, 완주한 선수의 이름
+    // return값 완주하지 못한 선수의 이름
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (String p : participant){
+            hashMap.put(p, hashMap.getOrDefault(p,0)+1);
+        }
+
+        for (String c : completion){
+            hashMap.put(c,hashMap.get(c) - 1);
+        }
+
+        for (String key : hashMap.keySet()){
+            if (hashMap.get(key) != 0){
+                answer = key;
+                break;
+            }
+        }
+        return answer;
+    }
+}
