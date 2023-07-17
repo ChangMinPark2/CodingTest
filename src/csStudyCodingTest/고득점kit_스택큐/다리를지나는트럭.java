ef787cfd74f88c5306db9e44ed406aa6106ce761
CHANG MIN PARK <98qkrckdals@naver.com>
2023-07-17 17:41:26 +0900

    고득점kit : 큐 -> 문제오류

--- src/csStudyCodingTest/고득점kit_스택큐/다리를지나는트럭.java
+++ src/csStudyCodingTest/고득점kit_스택큐/다리를지나는트럭.java
@@ -12,33 +12,43 @@ import java.util.Queue;
  */
 public class 다리를지나는트럭   {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int sum = 0;
+        int time = 0;
 
-        Queue<Integer> q = new LinkedList<>();
-        for (int i : truck_weights) q.add(i);
+        for(int i = 0; i < truck_weights.length; i++) {
+            int truck = truck_weights[i];
 
-        while (!q.isEmpty()){
-            int x = q.poll();
-            count++;
-            answer++;
-            //다리에 있는 조건
-            if (q.peek() == null){
-              answer ++;
-              break;
-            } else if (x + q.peek() <= weight && count <= bridge_length){
-                count--;
-                answer++;
-            } else { // 다리에 없고 건너는 조건
-                count =-1;
-                answer ++;
+            while(true) {
+                // 큐에 아무것도 없는 경우 = 어떠한 트럭도 다리위에 없음
+                if(queue.isEmpty()) {
+                    queue.add(truck);
+                    sum += truck;
+                    time++; // 다리에 오를 때만 시간 추가
+                    break;
+                } else if(queue.size() == bridge_length) { // 큐에 다리 길이만큼 트럭이 다 찬 경우
+                    sum -= queue.poll();
+                } else  { // 다리 길이만큼 큐가 차지않았음
+                    // weight 값을 넘지 않는 선에서 새로운 트럭을 다리에 올려줌
+                    if(sum + truck <= weight) {
+                        queue.add(truck);
+                        sum += truck;
+                        time++;
+                        break;
+                    } else {
+                        // 넘는다면 0을 넣어 이미 큐에 있는 트럭이 다리를 건너게 만듬
+                        queue.add(0);
+                        time++;
+                    }
+                }
             }
         }
-        return answer;
+        // 마지막 트럭에서 반복문이 끝나는데, 마지막 역시 다리 길이만큼 지나가야하기에 + 다리 길이
+        return time + bridge_length;
     }
 
     public static void main(String[] args) {
-        int[] a = {7, 4, 5, 6};
-        System.out.println(solution(2, 10, a));
+        int[] a = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        System.out.println(solution(100, 100, a));
     }
 }
---
+++ src/csStudyCodingTest/고득점kit_스택큐/주식가격.java
@@ -0,0 +1,8 @@
+package csStudyCodingTest.고득점kit_스택큐;
+
+public class 주식가격 {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+        return answer;
+    }
+}
