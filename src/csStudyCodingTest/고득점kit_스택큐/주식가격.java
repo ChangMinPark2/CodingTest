4d0b05953e56f73c90a0e8392b9f8871a3f8fef1
CHANG MIN PARK <98qkrckdals@naver.com>
2023-07-17 18:20:24 +0900

    고득점kit : 스택(이중반복문) -> 주식가격

--- src/csStudyCodingTest/고득점kit_스택큐/주식가격.java
+++ src/csStudyCodingTest/고득점kit_스택큐/주식가격.java
@@ -1,8 +1,25 @@
 package csStudyCodingTest.고득점kit_스택큐;
 
+import java.util.Arrays;
+
 public class 주식가격 {
-    public int[] solution(int[] prices) {
-        int[] answer = {};
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for (int i=0; i< prices.length; i++){
+            for (int j = i+1; j<answer.length; j++){
+                answer[i]++;
+
+                if (prices[i] > prices[j]) break;
+            }
+        }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[]a = {1, 2, 3, 2, 3};
+
+        System.out.println(Arrays.toString(solution(a)));
     }
 }
