ca7f1146d66527fc2e3df09ab60d8a22db20ff45
CHANG MIN PARK <98qkrckdals@naver.com>
2023-07-20 13:38:53 +0900

    레벨0

--- src/csStudyCodingTest/Level0/최빈값구하기.java
+++ src/csStudyCodingTest/Level0/최빈값구하기.java
@@ -1,6 +1,8 @@
 package csStudyCodingTest.Level0;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class 최빈값구하기 {
     public int solution(int[] array) {
@@ -25,5 +27,32 @@ public class 최빈값구하기 {
         }
 
         return answer;
+    }
+
+    public static int hash(int arr[]){
+        int count = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i : arr){
+            hashMap.put(i, hashMap.getOrDefault(i, 0) + 1);
+        }
+
+        int maxKey = Collections.max(hashMap.entrySet(), (o1, o2) -> o1.getValue() - o2.getValue()).getKey();
+        System.out.println(maxKey);
+
+        for (int key : hashMap.keySet()){
+            if (hashMap.get(key) == hashMap.get(maxKey)){
+                count++;
+            }
+        }
+
+        if (count > 1){
+            return -1;
+        } else return maxKey;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 2, 2, 2};
+        System.out.println(hash(arr));
     }
 }
