7e020b405f1f97688cabb80fc608134a1b9eed17
CHANG MIN PARK <98qkrckdals@naver.com>
2023-07-20 16:42:54 +0900

    고득점kit -> 힙(우선순위큐) 레벨2

---
+++ src/csStudyCodingTest/고득점kit_힙ㅡ우선순위큐/더맵게.java
@@ -0,0 +1,33 @@
+package csStudyCodingTest.고득점kit_힙ㅡ우선순위큐;
+
+import java.util.PriorityQueue;
+
+public class 더맵게 {
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i : scoville){
+            pq.add(i);
+        }
+        while (!pq.isEmpty()){
+            if (pq.peek() < K){
+                if (pq.size() < 2) {
+                    return -1;
+                }
+                int first = pq.poll();
+                int second = pq.poll();
+                pq.add(first + (second * 2));
+                answer++;
+            } else break;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        System.out.println(solution(a, 200));
+    }
+
+}
