c40da16b5c0d9bf0a47f170c22a35f80677f0380
CHANG MIN PARK <98qkrckdals@naver.com>
2023-08-07 12:10:40 +0900

    레벨 0

---
+++ src/csStudyCodingTest/Level0/첫번째로나오는음수.java
@@ -0,0 +1,13 @@
+package csStudyCodingTest.Level0;
+
+public class 첫번째로나오는음수 {
+    public int solution(int[] num_list) {
+
+        for(int i = 0; i<num_list.length; i++){
+            if (num_list[i] < 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
