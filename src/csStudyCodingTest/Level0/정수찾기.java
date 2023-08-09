93746929c5bd612afdf227c81ed529dfd7602cc4
CHANG MIN PARK <98qkrckdals@naver.com>
2023-08-09 16:33:24 +0900

    레벨0

--- src/csStudyCodingTest/Level0/정수찾기.java
+++ src/csStudyCodingTest/Level0/정수찾기.java
@@ -2,7 +2,7 @@ package csStudyCodingTest.Level0;
 
 public class 정수찾기 {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
+
         for(int i = 0; i<num_list.length; i++){
             if(num_list[i] == n){
                 return 1;
