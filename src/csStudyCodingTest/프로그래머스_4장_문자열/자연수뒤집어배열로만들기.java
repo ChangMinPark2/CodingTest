492bc460b78af458b8d6dfac64e9f74536df0c32
CHANG MIN PARK <98qkrckdals@naver.com>
2023-08-24 15:03:31 +0900

    레벨1

--- src/csStudyCodingTest/프로그래머스_4장_문자열/자연수뒤집어배열로만들기.java
+++ src/csStudyCodingTest/프로그래머스_4장_문자열/자연수뒤집어배열로만들기.java
@@ -1,19 +1,26 @@
 package csStudyCodingTest.프로그래머스_4장_문자열;
 
+import java.util.Arrays;
+
 public class 자연수뒤집어배열로만들기 {
-    public int[] solution(long n) {
+    public static int[] solution(long n) {
         int[] answer = {};
 
-        String str = String.valueOf(n);
-        String reversed = new StringBuilder(str).reverse().toString();
-        char[] arr = reversed.toCharArray();
+        String str = String.valueOf(n); // "12345"
+        System.out.println(str);
+        String reversed = new StringBuilder(str).reverse().toString(); //"54321"
 
-        answer = new int[arr.length];
+        answer = new int[reversed.length()];
 
-        for (int i = 0; i < arr.length; i++){
-            answer[i] = arr[i] = '0';
+        for (int i = 0; i < reversed.length(); i++){
+            answer[i] = reversed.charAt(i) - '0';
         }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        long a = 12345;
+        System.out.println(Arrays.toString(solution(a)));
     }
 }
