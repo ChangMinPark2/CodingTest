e9eda7aab0e67911eb38e3fa156e175b9f35f240
CHANG MIN PARK <98qkrckdals@naver.com>
2023-08-26 09:31:15 +0900

    레벨1

---
+++ src/csStudyCodingTest/Level1/가장가까운글자.java
@@ -0,0 +1,14 @@
+package csStudyCodingTest.Level1;
+
+public class 가장가까운글자 {
+    public int[] solution(String s) {
+        int[] answer = {};
+        String[] str = s.split("");
+        for (int i = 0; i < s.length(); i++){
+            for (int j = 0; j < i; j++){
+
+            }
+        }
+        return answer;
+    }
+}
