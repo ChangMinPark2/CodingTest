561652422ecd7d848463712ba4d0cdc015337bb1
CHANG MIN PARK <98qkrckdals@naver.com>
2023-09-03 00:33:34 +0900

    코딩테스트입문

--- src/csStudyCodingTest/코딩테스트입문/숫자찾기.java
+++ src/csStudyCodingTest/코딩테스트입문/숫자찾기.java
@@ -6,7 +6,7 @@ public class 숫자찾기 {
         String str = String.valueOf(num);
 
         for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == k + '0'){
+            if (str.charAt(i) == k + 48){
                 answer = i+1;
                 break;
             }
