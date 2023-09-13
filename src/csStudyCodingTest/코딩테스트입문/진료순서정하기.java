138d648d242fc91e87b2f126f218056451464713
CHANG MIN PARK <98qkrckdals@naver.com>
2023-09-14 01:38:09 +0900

    코딩테스트입문

---
+++ src/csStudyCodingTest/코딩테스트입문/진료순서정하기.java
@@ -0,0 +1,21 @@
+package csStudyCodingTest.코딩테스트입문;
+
+import java.util.*;
+
+public class 진료순서정하기 {
+
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+
+        for(int i = 0; i < emergency.length; i++){
+            for(int j = 0; j < emergency.length; j++){
+                if(emergency[i] <= emergency[j]){
+                    answer[i] += 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
+
