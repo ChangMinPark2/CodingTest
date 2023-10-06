d2d0b8bda3a6404f3cdf0fe91785a90724217d99
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-06 15:22:48 +0900

    프로그래머스입문

--- src/csStudyCodingTest/코딩테스트입문/옹알이일.java
+++ src/csStudyCodingTest/코딩테스트입문/옹알이일.java
@@ -21,12 +21,29 @@ public class 옹알이일 {
         }
         return answer;
     }
+    public static int solution2(String[] babbling) {
+        int answer = 0;
+
+        for(int i = 0; i < babbling.length; i++){
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+
+            if (babbling[i].isEmpty()){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 
     public static void main(String[] args) {
         String[] a = {"aya", "yee", "u", "maa", "wyeoo"};
         String c = "12345";
         String d = c.replace("0", "2");
         System.out.println(d);
-        System.out.println(solution(a));
+        System.out.println(solution2(a));
     }
 }
