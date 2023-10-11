063c2790267c3d34a448358b717a1cc040579700
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-12 02:41:43 +0900

    프로그래머스_문자열

---
+++ src/csStudyCodingTest/Level1/삼진법뒤집기.java
@@ -0,0 +1,9 @@
+package csStudyCodingTest.Level1;
+
+public class 삼진법뒤집기 {
+    public static void main(String[] args) {
+        String a = "1200";
+        int b = Integer.parseInt(a, 3);
+        System.out.println(b);
+    }
+}
