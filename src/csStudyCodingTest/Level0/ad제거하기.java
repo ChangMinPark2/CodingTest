6353cb70b63e3b61cdbc5a2aad4056a0b90e7c0c
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-17 00:49:21 +0900

    레벨0

---
+++ src/csStudyCodingTest/Level0/ad제거하기.java
@@ -0,0 +1,21 @@
+package csStudyCodingTest.Level0;
+
+import java.util.*;
+
+public class ad제거하기 {
+
+    public String[] solution(String[] strArr) {
+
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < strArr.length; i++){
+            if(!strArr[i].contains("ad")){
+                list.add(strArr[i]);
+            }
+        }
+
+        String[] answer = list.toArray(new String[0]);
+
+        return answer;
+    }
+}
