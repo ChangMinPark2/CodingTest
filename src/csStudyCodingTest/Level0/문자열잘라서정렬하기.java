644dac4668bdbf680e2437d5d1f5d90b354c3613
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-19 02:05:10 +0900

    레벨0

--- src/csStudyCodingTest/Level0/문자열잘라서정렬하기.java
+++ src/csStudyCodingTest/Level0/문자열잘라서정렬하기.java
@@ -1,13 +1,14 @@
 package csStudyCodingTest.Level0;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class 문자열잘라서정렬하기 {
 
-    public String[] solution(String myString) {
+    public static String[] solution(String myString) {
 
         String[] answer = myString.split("x");
         List<String> list = new ArrayList();
-
+        System.out.println(Arrays.toString(answer));
         for(String str : answer){
             if(!str.isEmpty()){
                 list.add(str);
@@ -18,5 +19,17 @@ public class 문자열잘라서정렬하기 {
         Arrays.sort(repo);
 
         return repo;
+    }
+
+    public static String[] solution2(String myString) {
+
+        String[] answer = myString.split("x");
+        Arrays.sort(answer);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("chrxyuixxrex")));
+        System.out.println(Arrays.toString(solution2("chrxyuixxrex")));
     }
 }
