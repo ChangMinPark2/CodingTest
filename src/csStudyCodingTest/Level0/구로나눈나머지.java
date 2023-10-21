3e11bf46689da606603bb6e9b6a69ec600ef6d3f
CHANG MIN PARK <98qkrckdals@naver.com>
2023-10-22 04:56:36 +0900

    레벨0

--- src/csStudyCodingTest/Level0/구로나눈나머지.java
+++ src/csStudyCodingTest/Level0/구로나눈나머지.java
@@ -1,5 +1,9 @@
 package csStudyCodingTest.Level0;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class 구로나눈나머지 {
 
     public int solution(String number) {
@@ -12,5 +16,30 @@ public class 구로나눈나머지 {
         answer = sum % 9;
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+//        while (true) {
+//            System.out.print("숫자를 입력해주세요 : ");
+//            Scanner scanner = new Scanner(System.in);
+//
+//            List<Integer> playerNumbers = new ArrayList<>();
+//            String input = scanner.next();
+//            if (input.equals("1")){
+//                System.out.println("굳");
+//            }
+//
+//            for (String number : input.split("")) {
+//                playerNumbers.add(Integer.parseInt(number));
+//            }
+//
+//            int i = 0;
+//            i++;
+//            if(i > 3){
+//                break;
+//            }
+//        }
+
+        System.out.println("안녕");
     }
 }
