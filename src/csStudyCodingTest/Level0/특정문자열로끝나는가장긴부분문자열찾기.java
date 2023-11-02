96688c8e7f726dcaabf7ddd9cf48f18d0e64f7c9
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-03 04:19:56 +0900

    레벨0

---
+++ src/csStudyCodingTest/Level0/특정문자열로끝나는가장긴부분문자열찾기.java
@@ -0,0 +1,8 @@
+package csStudyCodingTest.Level0;
+
+public class 특정문자열로끝나는가장긴부분문자열찾기 {
+    public String solution(String myString, String pat) {
+
+        return myString.substring(0, myString.lastIndexOf(pat)) + pat;
+    }
+}
