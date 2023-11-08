92c147d2ca6081bf36e51b7da590f9682c7e0854
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-08 22:49:42 +0900

    테스트

---
+++ src/csStudyCodingTest/dnddkgkstest.java
@@ -0,0 +1,24 @@
+package csStudyCodingTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class dnddkgkstest {
+
+    private static String abcddd = "abc";
+
+    public static void main(String[] args) {
+        String a = ",a,b,c,";
+
+        String[] b = a.split(",");
+
+        System.out.println(Arrays.toString(b));
+
+    }
+
+    public static String abc(){
+        System.out.println("안녕");
+        return abcddd;
+    }
+}
