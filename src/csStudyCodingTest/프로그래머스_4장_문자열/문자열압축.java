d37ba33d71c8a96aaad8438eaf9a5a086424b364
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-09 15:29:58 +0900

    레벨0

--- src/csStudyCodingTest/프로그래머스_4장_문자열/문자열압축.java
+++ src/csStudyCodingTest/프로그래머스_4장_문자열/문자열압축.java
@@ -1,4 +1,17 @@
 package csStudyCodingTest.프로그래머스_4장_문자열;
 
+/**
+ * 문제풀이흐름 (모든 길이에 대해 압축을 시도한 후 그중 가장 짧은 길이를 선택한다.)
+ * 1. 1부터 입력 문자열 s의 길이만큼 자를 문자열의 길이를 설정하여 반복한다.
+ * 설정된 길이만큼 문자열을 잘라 낸 token의 배열을 생성한다.
+ * 문자열을 비교하며 token의 배열을 하나의 문자열로 압축한다.
+ * 1 ~ 3 의 과정으로 압축된 문자열 중 가장 짧은 길이를 반환한다.
+ */
 public class 문자열압축 {
+    public static void main(String[] args) {
+        String a = "123456789";
+        //pass
+        int min = Integer.MAX_VALUE;
+
+    }
 }
