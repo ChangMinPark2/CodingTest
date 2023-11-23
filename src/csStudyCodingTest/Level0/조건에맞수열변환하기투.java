be11e56d38a6e91c1a26f104b80d1ef7f64b992a
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-24 04:32:40 +0900

    레벨0

---
+++ src/csStudyCodingTest/Level0/조건에맞수열변환하기투.java
@@ -0,0 +1,33 @@
+package csStudyCodingTest.Level0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class 조건에맞수열변환하기투 {
+    public int solution(int[] arr) {
+        int answer = 0;
+        int count = 0;
+        int[] newArr = new int[arr.length];
+
+        while (count != arr.length) {
+            count = 0;
+            newArr = arr.clone();
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 == 0 && arr[i] >= 50) {
+                    arr[i] /= 2;
+                } else if (arr[i] % 2 != 0 && arr[i] < 50) {
+                    arr[i] = arr[i] * 2 + 1;
+                }
+                if (arr[i] == newArr[i]) {
+                    count++;
+                }
+            }
+            answer++;
+        }
+
+        return answer - 1;
+    }
+}
