87cf0cc92a023a1fe0e032f9d2b2c1ec31d838a0
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-23 23:24:14 +0900

    레벨0

--- src/csStudyCodingTest/Level0/큐알코드.java
+++ src/csStudyCodingTest/Level0/큐알코드.java
@@ -1,5 +1,8 @@
 package csStudyCodingTest.Level0;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class 큐알코드 {
     public String solution(int q, int r, String code) {
@@ -11,5 +14,11 @@ public class 큐알코드 {
             }
         }
         return sb.toString();
+    }
+    public String solution2(int q, int r, String code) {
+        return IntStream.range(0, code.length()).
+                filter(operand -> operand % q == r).
+                mapToObj(i -> String.valueOf(code.charAt(i))).
+                collect(Collectors.joining());
     }
 }
