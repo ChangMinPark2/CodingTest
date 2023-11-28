e0f55c20ae83a98c41b0115d2e584e6e7d9e2edb
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-29 01:54:08 +0900

    레벨0

---
+++ src/csStudyCodingTest/Level0/수열과구간쿼리투.java
@@ -0,0 +1,30 @@
+package csStudyCodingTest.Level0;
+import java.util.*;
+
+public class 수열과구간쿼리투 {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+
+        for(int i = 0; i < queries.length; i++){
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            int min = Integer.MAX_VALUE;
+            for(int j = s; j <= e; j++){
+                if(arr[j] > k && arr[j] < min){
+                    min = arr[j];
+                }
+                answer[i] = min == Integer.MAX_VALUE ? -1 : min;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 4, 3};
+        int[][] arrr = {{0, 4, 2}, {0, 3, 2}, {0, 2, 2}};
+
+        System.out.println(solution(arr, arrr));
+    }
+}
