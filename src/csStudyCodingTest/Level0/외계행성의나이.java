1fba661f896db2620eaf8dca88d66afd321f747a
CHANG MIN PARK <98qkrckdals@naver.com>
2023-12-12 02:40:41 +0900

    레벨0: refactor

--- src/csStudyCodingTest/Level0/외계행성의나이.java
+++ src/csStudyCodingTest/Level0/외계행성의나이.java
@@ -2,13 +2,13 @@ package csStudyCodingTest.Level0;
 
 public class 외계행성의나이 {
     public String solution(int age) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         String age962 = "abcdefghij";
         String[] ageArr = String.valueOf(age).split("");
 
         for (int i = 0; i < ageArr.length; i++) {
-            answer += age962.charAt(Integer.valueOf(ageArr[i]));
+            sb.append(age962.charAt(Integer.valueOf(ageArr[i])));
         }
-        return answer;
+        return sb.toString();
     }
 }
