1e1517aa1e8a39e68750b97e844be5191a1a3937
CHANG MIN PARK <98qkrckdals@naver.com>
2023-12-20 17:36:36 +0900

    feat(7장_문자열) : commit Level 1

--- src/csStudyCodingTest/프로그래머스_7장_문자열/문자열내림차순으로배치하기.java
+++ src/csStudyCodingTest/프로그래머스_7장_문자열/문자열내림차순으로배치하기.java
@@ -6,7 +6,17 @@ public class 문자열내림차순으로배치하기 {
                 .boxed()
                 .sorted((o1, o2) -> o2 - o1)
                 .collect(StringBuilder::new,
-                         StringBuilder::appendCodePoint,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
+    }
+
+    public String solution2(String s) {
+        return s.chars()
+                .boxed()
+                .sorted((o1, o2) -> o2 - o1)
+                .collect(StringBuilder::new, //StringBuilder 선언
+                        StringBuilder::appendCodePoint, //	유니코드 코드 포인트를 덧붙입니다.
                         StringBuilder::append)
                 .toString();
     }
