2ba70baedb10edff68b0e4e03a83efa51f87d703
CHANG MIN PARK <98qkrckdals@naver.com>
2023-12-26 23:03:09 +0900

    feat(level 1)

--- src/csStudyCodingTest/Level1/옹알이투.java
+++ src/csStudyCodingTest/Level1/옹알이투.java
@@ -1,4 +1,5 @@
 package csStudyCodingTest.Level1;
+
 import java.util.*;
 
 public class 옹알이투 {
@@ -6,9 +7,9 @@ public class 옹알이투 {
         String[] a = {"aya", "ye", "woo", "ma"};
         int answer = 0;
 
-        for(int i = 0; i < babbling.length; i++){
-            for(int j = 0; j < a.length; j++){
-                if(babbling[i].contains("ayaaya") ||
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (babbling[i].contains("ayaaya") ||
                         babbling[i].contains("yeye") ||
                         babbling[i].contains("woowoo") ||
                         babbling[i].contains("mama")) {
@@ -18,8 +19,33 @@ public class 옹알이투 {
             }
         }
 
-        for(int i = 0; i < babbling.length; i++){
-            if(babbling[i].matches("\\d+")){
+        for (int i = 0; i < babbling.length; i++) {
+            if (babbling[i].matches("\\d+")) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public int solution2(String[] babbling) {
+        int answer = 0;
+
+        for (int i = 0; i < babbling.length; i++) {
+            if (babbling[i].contains("ayaaya") ||
+                    babbling[i].contains("yeye") ||
+                    babbling[i].contains("woowoo") ||
+                    babbling[i].contains("mama")) {
+                continue;
+            }
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+        }
+
+        for (int i = 0; i < babbling.length; i++) {
+            if (babbling[i].matches("\\d+")) {
                 answer++;
             }
         }
