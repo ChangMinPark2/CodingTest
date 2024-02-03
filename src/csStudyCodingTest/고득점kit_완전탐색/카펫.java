7d2a9472606249431e4158d91455fc5751790f51
CHANG MIN PARK <98qkrckdals@naver.com>
2024-02-03 23:45:37 +0900

    feat(programmers) : level2

--- src/csStudyCodingTest/고득점kit_완전탐색/카펫.java
+++ src/csStudyCodingTest/고득점kit_완전탐색/카펫.java
@@ -1,5 +1,10 @@
 package csStudyCodingTest.고득점kit_완전탐색;
 
+/**
+ * 테두리 갈색 격자와 나머지 노란색 격자의 개수를 통하여 가로와 세로를 구하는 문제.
+ * brown의 최솟값 8, yellow의 최솟값 1 -> 가로 세로의 최솟값은 3이다.
+ * 이중반복문 3,3 으로 시작하여 brown과 yellow의 개수가 같다면 리턴한다.
+ */
 public class 카펫 {
     public int[] solution(int brown, int yellow) {
         for (int x = 3; x < brown; x++) {
